cd5b09d60c3523a987b81efcdc97839b5eab9473
lalomarquez <alfa_8581@hotmail.com>
2017-07-04 17:55:31 -0500

    Part 40 - Using displayname, displayformat, scaffoldcolumn attributes in asp.net mvc application

---
+++ GuiaMVC4/Controllers/TemplatesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GuiaMVC4.Models;
+
+namespace GuiaMVC4.Controllers
+{
+    public class TemplatesController : Controller
+    {
+        //
+        // GET: /Templates/Details
+        public ActionResult Details(int id)
+        {
+            SampleDBContext db = new SampleDBContext();
+            tblEmployee emp = db.tblEmployee.Single(x => x.Id == id);
+
+            return View(emp);
+        }
+
+    }
+}
---
+++ GuiaMVC4/Models/Empleado.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GuiaMVC4.Models
+{
+    [MetadataType(typeof(Empleado))]
+    public class Empleado
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [ReadOnly(true)]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddres { get; set; }
+
+        [ScaffoldColumn(true)]
+        [DataType(DataType.Currency)]
+        public int? Salry { get; set; }        
+
+        [DataType(DataType.Url)]
+        [UIHint("OpenInNewWindow")]
+        public string PersonalWebSite { get; set; }
+
+        [DisplayAttribute(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:d}")]
+        public DateTime? HireDate { get; set; }
+
+        [DisplayFormat(NullDisplayText = "Gender not specified")]
+        public string Gender { get; set; }
+    }
+}
